76af034e469c230b4d8be54703114859c232ddde
SORA0920 <sora02230112@gmail.com>
2017-11-23 03:15:11 +0900

    大胆な仕様変更は個人開発の特権です！

--- toot.rb
+++ toot.rb
@@ -2,7 +2,41 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-def PostToot(text, vis, cw, account)  
+def post_toot(text, cw, account, config)  
+  activity :system, "test"
+
+
+  vis = 
+    case config
+      when 0 then
+        vis = "public"
+      when 1 then
+        vis = "unlisted"
+      when 2 then
+        vis = "private"
+      when 3 then
+        vis = "direct"
+      when 4 then
+        case random.rand(1..400)
+        when 1..100 then
+          vis = "public"
+        when 101..200 then
+          vis = "unlisted"
+        when 201..300  then
+          vis = "private"
+        when 301..400 then
+          vis = "direct"
+        else
+          activity :system, "0から3までの乱数が0から3以外の数値を出したよ！\nすごいね！どう考えてもバグだね！"
+        end
+      else
+        vis = "public"
+      end
+  
+  
+
+  
+  
   uri = URI.parse(account[:status_url])
   https = Net::HTTP.new(uri.host, uri.port)
   https.use_ssl = true
