25bc8a70393d6ce79d62b482c94925f26540a5e4
SORA0920 <sora02230112@gmail.com>
2017-11-27 14:01:21 +0900

    modelの実装、不要なコードの削除

--- mastodon.rb
+++ mastodon.rb
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
+require "thread"
 require_relative './toot'
-require_relative './model/site'
-require_relative './model/item'
+require_relative './model.rb'
 
 
 Plugin.create(:mastodon) do
-  # ランダム公開範囲用の乱数
+# ランダム公開範囲用乱数
   random = Random.new
   cw  = ""
   vis = "public"
 
+
+  filter_extract_datasources do |ds|
+    [ds.merge(mastodon: 'Mastodon')]
+  end
 
   tl = "home"
 
@@ -24,76 +28,102 @@ Plugin.create(:mastodon) do
     select("公開範囲", :mastodon_vis, { 0 => "公開", 1 => "非収載", 2 => "非公開", 3 => "ダイレクト" , 4=> "Random"})
   end
 
+# アカウントの配列
   account = {
     :token => UserConfig[:account_token],
     :host => UserConfig[:account_host]
   }
 
-  # タイムラインを作る
+# タイムラインを作る
   tab :mastodon_home, 'HomeTimeline' do
     set_icon "https://#{account[:host]}/favicon.ico"
     timeline :mastodon_home
   end  
   
-  # CWで投稿するコマンド
+
+# CWで投稿するコマンド
   command(:mastodon_cw,
           name: "CWで投稿",
           condition: lambda{ |opt| true },
           visible: true,
           role: :postbox) do |opt|
     cw_text = UserConfig[:cw_text]
-
-    # もしコンフィグ上のCWテキストが空だった場合には警告文を追加する
+  # もしコンフィグ上のCWテキストが空だった場合には警告文を追加する
     if cw_text.empty?
       cw_text = "閲覧注意！"
     end
-
+   #投稿欄から文字列を取得しToot 
     text = Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text 
     post_toot(text, cw_text, account, UserConfig[:mastodon_vis])    
-
+    # 投稿欄を空に
     Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = ""
   end
 
-
-
-  # 投稿欄を乗っ取る
+# 投稿欄を乗っ取る
   filter_gui_postbox_post do |gui_postbox, opt|
     text = Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer.text
-
-    # 投稿する
+  # もし投稿内容が正規表現なら警告する機能(未実装)    
+    if text =~ /^s\/.+\/.+\/?$/
+#      req_warn = Gtk::Dialog.new
+#
+#
+#      label_str = Gtk::Label.new("失った信頼はもう戻ってきませんが、本当にこの文章を投稿しますか？\n\n#{text}")
+#
+#      label_str.show
+#
+#      req_warn.vbox.pack_start(label_str, true, true, 30)
+#
+#
+#      req_warn.add_buttons(["No", Gtk::Dialog::RESPONSE_NO],["Yes", Gtk::Dialog::RESPONSE_YES])
+#
+#      req_warn.signal_connect("response") do |widget, responsei|
+#        case response
+#        when Gtk::Dialog::RESPONSE_YES
+#          post_toot(text, cw, account, UserConfig[:mastodon_vis])
+#        when Gtk::Dialog::RESPONSE_NO
+#          activity :system, "test"
+#        end
+#      end
+#      req_warn.show_all
+#    else
+      activity :system, "正規表現だよ！"
+    end
+#   投稿する
     post_toot(text, cw, account, UserConfig[:mastodon_vis])
-
+#    end
     # 投稿欄を空に
     Plugin.create(:gtk).widgetof(gui_postbox).widget_post.buffer.text = ""
-
-#    試してるコードをコメントアウト
-#    Thread.new{  
-#      JSON.parse(res.body)
-#   
-#      activity :system,  "test"
-#    }.next{|toot|
-#      result = toot
-#      activity :system,  "test"
-#      [Plugin::Mastodon::Site.new(name: result[account[display_name]],
-#                                  description: result[account[note]],
-#                                  link: result[account[url]],
-#                                  created: result[account[created_at]],
-#                                  feed_url: result[account[url]],
-#                                  profile_image_url: result[account[avatar]]), user]
-#    }.next{|site, user, result|
-#      activity :system, "test"
-#      Plugin::Mastodon::Item.new(guid: result[id],
-#                                 link: result[url],
-#                                 title: "null",
-#                                 description: result[content],
-#                                 created: result[created_at],
-#                                 site: site)
-#    }.next{|item|
-#      activity :system,  "test"
-#      timeline(:mastodon_home) << item
-#    }
-
   end
+
+# タイムラインにTootを追加するテスト(タイムラインに固定の内容の投稿を追加することには成功してます)
+  def test_1 
+#    activity :system, "!" 
+    user = MstdnUser.new_ifnecessary(
+      name: "TestUser",
+      link: "https://mstdn.maud.io/@Non",
+      created: Time.at(0),
+      profile_image_url: "https://mstdn.maud.io/favicon.ico",
+      id: 1
+    )
+#    activity :system, "user!"
+    toot = MstdnToot.new_ifnecessary(
+      id: 1,
+      link: "https://mstdn.maud.io/favicon.ico",
+      description: "てすと！",
+      created: Time.at(0),
+      user: user
+    )
+#    activity :system, "toot!"
+    
+    return toot
+  end
+# タイムラインにTootを追加
+  on_period { |service|
+    toots = []
+    toots.push(test_1)
+    toots.push(test_1)
+    timeline(:mastodon_home) << toots
+  }
 
   on_boot do |service|
   end
---
+++ model.rb
@@ -0,0 +1,23 @@
+class MstdnUser < Retriever::Model
+  include Retriever::Model::UserMixin
+  
+  register :mstdn_user, name: "Mastodon Timeline User"
+  field.string :name, required: true
+  field.string :link
+  field.time   :created
+  field.string :profile_image_url
+  field.int    :id
+end
+
+class MstdnToot < Retriever::Model
+  include Retriever::Model::MessageMixin
+
+  register :mstdn_toot, name: "Mastodon Timeline Toot"
+
+  field.int    :id
+  field.string :link
+  field.string :description
+  field.time   :created
+  field.has    :user, MstdnUser, required: true
+end
+
