f9debea626481433e8958ceefebedd23218776a1
ahiru <wellingtonnmaeda0524@gmail.com>
2017-12-26 22:39:53 +0900

    インデント修正

--- orig_parse.rb
+++ orig_parse.rb
@@ -1,7 +1,7 @@
 def parse_name(data)
-  if data["account"] ["display_name"].empty?
-    return data["account"] ["username"]
+  if data["account"]["display_name"].empty?
+    return data["account"]["username"]
   else
-    return data["account"] ["display_name"]
+    return data["account"]["display_name"]
   end
 end
