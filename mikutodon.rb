45fae17e0bb78d1c97a49a68508fcf0a714db386
ahiru <wellingtonnmaeda0524@gmail.com>
2017-12-27 08:03:30 +0900

    post時のエラー処理追加
    activityに表示するだけ

--- mikutodon.rb
+++ mikutodon.rb
@@ -123,7 +123,10 @@ Plugin.create(:mikutodon) do
 
     text = Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text
     post_toot(text, cw_text, account, UserConfig[:mastodon_vis]).next {
+      # 投稿成功時のみバッファをクリア
       Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = ""
+    }.trap { |err|
+      error err
     }
   end
 
@@ -165,7 +168,10 @@ Plugin.create(:mikutodon) do
     end
 
     post_toot(text, cw, account, UserConfig[:mastodon_vis]).next {
+      # 投稿成功時のみバッファをクリア
       Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = ""
+    }.trap { |err|
+      error err
     }
 
 
