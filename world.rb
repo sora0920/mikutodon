4d2b2fab1b8bd0a46cef058d197e536b23f18320
SORA0920 <sora02230112@gmail.com>
2017-12-27 14:15:08 +0900

    選択するまではクラッシュしない。つまりは神

--- world.rb
+++ world.rb
@@ -1,6 +1,7 @@
 class World < Diva::Model
   register :mikutodon, name: "Mastodon"
 
+  field.string :id, required: true
   field.string :slug, required: true
   field.string :name, required: true
   field.string :host, required: true
@@ -9,11 +10,27 @@ class World < Diva::Model
   def self.build(token, host)
     user = get_user("verify_credentials", {token: token, host: host})[:body]
     self.new(
-      slug: "mastodon#{user["id"]}",
+      id: user["id"],
+      slug: "mastodon #{user["username"]}",
       name: user["username"],
       host: host,
       token: token
     )
+  end
+
+  def title
+    "#{self.name}@#{self.host}"
+  end
+
+  def icon
+    get_user("verify_credentials", {token: self.token, host: self.host})[:body]["avatar"]
+  end
+
+  def to_hash
+    super.merge(user: {id: self.id,
+                       idname: "#{self.name}@#{self.host}",
+                       name: self.name,
+                       profile_image_url: self.icon})
   end
 end
 
