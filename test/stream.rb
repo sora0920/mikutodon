62ac398450a204ad10fef7df680f17a7ea0b0af6
SORA0920 <sora02230112@gmail.com>
2018-02-04 20:12:17 +0900

    SSEのパースを仮実装

--- test/stream.rb
+++ test/stream.rb
@@ -1,20 +1,45 @@
 require "net/http"
 require "uri"
-require "open-uri"
 
 def get_sse(host, token)
   uri = URI.parse("https://#{host}/api/v1/streaming/user")
+  buffer = ""
 
   Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |https|
     req = Net::HTTP::Get.new(uri)
     req["Authorization"] = "Bearer #{token}"
 
     https.request(req) do |res|
-      res.read_body do |body|
-        puts body
+      res.read_body do |chunk|
+        buffer += chunk
+        while index = buffer.index(/\r\n\r\n|\n\n/)
+          stream = buffer.slice!(0..index)
+          puts sse_parse(stream)
+        end
       end
     end
   end
+
+end
+
+def sse_parse(stream)
+  data = ""
+  name = nil
+
+  stream.split(/\r?\n/).each do |part|
+    /^data:(.+)$/.match(part) do |m|
+      data += m[1].strip
+      data += "\n"
+    end
+    /^event:(.+)$/.match(part) do |m|
+      name = m[1].strip
+    end
+  end
+
+  return {
+    event: name,
+    body: data.chomp!
+  }
 
 end
 
